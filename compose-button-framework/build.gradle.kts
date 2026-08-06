c612bb23e6bbc1e7d31e3407edb87e7948b8da27
ved-prakash-coca-cola-acount <pved@coca-cola.com>
2026-08-06 17:08:43 +0530

    Release 1.0.1

--- compose-button-framework/build.gradle.kts
+++ compose-button-framework/build.gradle.kts
@@ -36,7 +36,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.vedprakashsah1998"
             artifactId = "compose-button-framework"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
